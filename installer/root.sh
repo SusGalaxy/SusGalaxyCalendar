81864c49a02ee6bc5306b262002a617974382835
Iniko Matthew Jemima <109912460+InikoMatthewPro@users.noreply.github.com>
2023-02-18 15:05:46 +0530

    Update root.sh

--- installer/root.sh
+++ installer/root.sh
@@ -2,7 +2,7 @@ sudo apt update -y
 sudo apt full-upgrade -y
 sudo apt install -y yad git wget 
 
-mkdir $HOME/susgalaxy
+mkdir $HOME/susgalaxyapps
 wget https://raw.githubusercontent.com/SusGalaxy/SusGalaxyCalendar/main/gui
 wget https://github.com/SusGalaxy/SusGalaxyCalendar/raw/main/susgalaxy.png
 
@@ -10,15 +10,14 @@ mkdir -p ~/.local/share/applications
 echo "[Desktop Entry]
 Name=Sus Galaxy Calendar
 Comment=Calendar GUI for Linux
-Exec=${DIRECTORY}/gui
-Icon=${DIRECTORY}/susgalaxy.png
+Exec=$HOME/susgalaxyapps/gui
+Icon=$HOME/susgalaxyapps/susgalaxy.png
 Terminal=false
 StartupWMClass=Sus-Galaxy-Calendar
 Type=Application
 Categories=Utility;
 StartupNotify=true" > ~/.local/share/applications/susgalaxycalendar.desktop
 chmod 755 ~/.local/share/applications/susgalaxycalendar.desktop
-gio set ~/.local/share/applications/susgalaxycalendar.desktop "metadata::trusted" yes
 
 #copy menu button to Desktop
 cp -f ~/.local/share/applications/susgalaxycalendar.desktop ~/Desktop
